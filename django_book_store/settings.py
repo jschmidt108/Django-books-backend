8bd871a88901f30a911bc2481178f5966b1a2a86
Jacqueline Schmidt <jschmidt108@gmail.com>
2022-06-25 16:34:45 -0400

    heroku deployment

--- django_book_store/settings.py
+++ django_book_store/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'lit-spire-95226.herokuapp.com']
 
 
 # Application definition
