767b4d90052d92c4ed79063604f2f686dd7e892e
Jacqueline Schmidt <jschmidt108@gmail.com>
2022-06-27 14:29:43 -0400

    heroku deploy

--- auth_api/models.py
+++ auth_api/models.py
@@ -5,13 +5,13 @@ from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
 class UserAccount(models.Model):
-    name = models.CharField(max_length=200, default= 'user')
+    name = models.CharField(max_length=200)
     username = models.CharField(max_length=200, unique=True)
     email = models.CharField(max_length=75, unique=True)
     password = models.CharField(max_length=1000)
-    hasRead = models.ManyToManyField(Book, related_name='books_read', blank=True, null=True)
-    isReading = models.ManyToManyField(Book, related_name='books_reading', blank=True, null=True)
-    toRead = models.ManyToManyField(Book, related_name='books_wantToRead', blank=True, null=True)
+    hasRead = models.ManyToManyField(Book, related_name='books_read', blank=True)
+    isReading = models.ManyToManyField(Book, related_name='books_reading', blank=True)
+    toRead = models.ManyToManyField(Book, related_name='books_wantToRead', blank=True)
     # userReviews = models.ManyToManyField(Review)
 
 
