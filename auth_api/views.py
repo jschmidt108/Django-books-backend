f342a4c488225b89e5f07326a41e0e73bd63f0c9
Jacqueline Schmidt <jschmidt108@gmail.com>
2022-07-05 15:31:46 -0400

    heroku deployment

--- auth_api/views.py
+++ auth_api/views.py
@@ -38,7 +38,7 @@ def check_login(request):
         if UserAccount.objects.get(email=email): #see if email exists in db
             user = UserAccount.objects.get(email=email)  #find user object with matching email
             if check_password(password, user.password): #check if passwords match
-                return JsonResponse({'id': user.id, 'name': user.name, 'username': user.username, 'email': user.email}) #if passwords match, return a user dict
+                return JsonResponse({'id': user.id, 'name': user.name, 'username': user.username, 'email': user.email, 'hasRead': user.hasRead, 'isReading': user.isReading, 'toRead': user.toRead}) #if passwords match, return a user dict
             else: #passwords don't match so return empty dict
                 return JsonResponse({'p'})
         else: #if email doesn't exist in db, return empty dict
